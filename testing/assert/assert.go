c37e8c16e0c2cb49f797a74453fd0e4a0e1e5c6b
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2024-03-17 11:02:37 -0300

    refactor and new functions
    can use personalized feedbacks now

--- testing/assert/assert.go
+++ testing/assert/assert.go
@@ -1,71 +1,101 @@
 package assert
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
-func NotNil(t testing.TB, v any) {
-	t.Helper()
+func output(common string, out []any) string {
+	if len(out) == 0 {
+		return common
+	}
+	if str, ok := out[0].(string); ok {
+		return fmt.Sprintf(str, out[1:]...)
+	}
+	panic("output argument must be a fmt string")
+}
 
+func NotNil(t testing.TB, v any, out ...any) {
+	t.Helper()
 	if v == nil {
-		t.Fatal("expected not nil")
+		t.Fatal(output("expected not nil value", out))
 	}
 }
 
-func NotEmpty[T any](t testing.TB, v T) {
+func Nil(t testing.TB, v any, out ...any) {
 	t.Helper()
 
-	value := reflect.ValueOf(v)
-	kind := value.Kind()
+	if v != nil {
+		t.Fatal(output("expected nil value", out))
+	}
+}
 
-	if kind == reflect.Pointer {
-		value = value.Elem()
-		kind = value.Kind()
+func isEmpty(v any) bool {
+	if v == nil {
+		return true
 	}
 
-	switch kind {
+	value := reflect.ValueOf(v)
+
+	switch value.Kind() {
 	case reflect.Array,
 		reflect.Chan,
 		reflect.Map,
 		reflect.Slice,
 		reflect.String:
-		if value.Len() != 0 {
-			return
+		return value.Len() == 0
+	case reflect.Ptr:
+		if value.IsNil() {
+			return true
 		}
+		return isEmpty(value.Elem().Interface())
 	default:
-		zeroValue := reflect.Zero(reflect.TypeOf(value))
-		if !reflect.DeepEqual(zeroValue, value) {
-			return
-		}
+		zero := reflect.Zero(value.Type())
+		return reflect.DeepEqual(value, zero)
 	}
-	t.Fatalf("expected not empty")
 }
 
-func Equal[T comparable](t testing.TB, got, want T) {
+func Empty[T any](t testing.TB, v T, out ...any) {
 	t.Helper()
 
-	if got != want {
-		t.Fatalf("expected same values, but got %v and want %v", got, want)
+	if !isEmpty(v) {
+		common := fmt.Sprintf("expected empty, but got %v", v)
+		t.Fatal(output(common, out))
 	}
 }
 
-func NoError(t testing.TB, err error) {
+func NotEmpty[T any](t testing.TB, v T, out ...any) {
+	t.Helper()
+
+	if isEmpty(v) {
+		common := fmt.Sprintf("expected not empty, but got %v", v)
+		t.Fatal(output(common, out))
+	}
+}
+
+func Equal[T any](t testing.TB, got, want T, out ...any) {
+	t.Helper()
+
+	if !reflect.DeepEqual(got, want) {
+		common := fmt.Sprintf("expected equal values, but got %v and want %v", got, want)
+		t.Fatal(output(common, out))
+	}
+}
+
+func NoError(t testing.TB, err error, out ...any) {
 	t.Helper()
 
 	if err != nil {
-		t.Fatalf("expect no error, got %v", err)
+		common := fmt.Sprintf("expect no error, got %v", err)
+		t.Fatal(output(common, out))
 	}
 }
 
-func Error(t testing.TB, got, want error) {
+func Error(t testing.TB, err error, out ...any) {
 	t.Helper()
 
-	if got == nil {
-		t.Fatalf("expect error, but didn't got one")
-	}
-
-	if got != want {
-		t.Fatalf(`got error "%v" but want "%v"`, got, want)
+	if err == nil {
+		t.Fatal(output("expect error, but didn't got one", out))
 	}
 }
